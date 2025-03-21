0f74b2a1b6cbd43275fb1d00ad310a17f18e6a74
Anton S <hitman.soldirs@gmail.com>
2025-03-21 20:31:55 +0800

    The first change 1

--- src/Main.java
+++ src/Main.java
@@ -10,22 +10,22 @@ public class Main {
         int[] salaryComp = {39_900, 29_000, 19_120, 19_190, 9_900};
         int minSalary = 99999999;
         int maxSalary = 0;
-        for (int i = 0; i < salaryComp.length; i++) {
-            if (salaryComp[i] > maxSalary) {
-                maxSalary = salaryComp[i];
+        for (int j : salaryComp) {
+            if (j > maxSalary) {
+                maxSalary = j;
             }
         }
         System.out.println("Максимальная сумма трат за неделю составила " + maxSalary + " рублей");
-        for (int i = 0; i < salaryComp.length; i++) {
-            if (salaryComp[i] < minSalary) {
-                minSalary = salaryComp[i];
+        for (int j : salaryComp) {
+            if (j < minSalary) {
+                minSalary = j;
             }
         }
         System.out.println("Минимальная сумма трат за неделю составила " + minSalary + " рублей");
         System.out.println("Задание 3");
-        int[] salaryPipls = {56_900, 45_000, 34_120, 12_190, 159_900};
-        double sumPipl = Arrays.stream(salaryPipls).sum();
-        double average = sumPipl / salaryPipls.length;
+        int[] salaryPeople = {56_900, 45_000, 34_120, 12_190, 159_900};
+        double sumPeople = Arrays.stream(salaryPeople).sum();
+        double average = sumPeople / salaryPeople.length;
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
         System.out.println("Задание 4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
@@ -33,6 +33,5 @@ public class Main {
             System.out.print(reverseFullName[i]);
         }
         System.out.println(" "); // новый вывод кода с новой строки
-
     }
 }
